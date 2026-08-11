b8712f9945fec99e8347cf9c1ee2eaf653e2c2cd
Jakub Cieślik <kuba8172@gmail.com>
2026-08-11 17:18:44 +0200

    feat: hero section

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,15 +1,13 @@
-import { useTranslations } from 'next-intl';
+import { Hero } from '@/components/hero';
 import { Intro } from '@/components/intro';
 import { Navbar } from '@/components/navbar/navbar';
 
 export default function Home() {
-  const t = useTranslations('home');
-
   return (
     <>
       <Intro />
       <Navbar />
-      <main id="home">{t('title')}</main>
+      <Hero />
     </>
   );
 }
