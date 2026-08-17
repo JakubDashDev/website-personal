0633259aed19723f2b3976b25c58a9325a95fc10
Jakub Cieślik <kuba8172@gmail.com>
2026-08-17 14:54:49 +0200

    feat: reduce size of hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
-import heroImage from '@/public/hero.png';
+import heroImage from '@/public/hero.webp';
 
 export function Hero() {
   const t = useTranslations('home');
@@ -13,6 +13,7 @@ export function Hero() {
           alt=""
           className="hero-image-fade order-2 hidden w-full mix-blend-screen lg:block"
           sizes="(min-width: 1024px) 45vw, 1px"
+          loading='eager'
         />
         <div>
           <h1 className="text-5xl leading-none font-medium tracking-tighter sm:text-6xl lg:text-8xl">
